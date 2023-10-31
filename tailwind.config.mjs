6e6555af4cc6f556987c39069ad80c14b9859c13
Ian Mlolwa <ian@debian.deb.debian.org>
2023-10-31 03:29:53 +0300

    initial push

---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			"primaryBtn-blue": "#111B47",
+			"landingText": "#37447E",
+			"white": "#ffffff",
+		},
+		fontSize: {
+			"50": "50px",
+			"18": "18px",
+			"16": "16px",
+			"26": "26px"
+		},
+		fontFamily: {
+			roboto: ["roboto"]
+		},
+		lineHeight: {
+			"18": "18px"
+		}
+	},
+	plugins: [],
+}
